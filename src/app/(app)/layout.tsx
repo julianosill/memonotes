00e9b72a14430a3cc9cb27ab00011171344079b8
Juliano Sill <sill.juliano@gmail.com>
2024-03-10 20:36:42 -0300

    feat(components): create search-form

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from 'react'
 
+import { SearchForm } from '@/components/search-form'
 import { Sidebar } from '@/components/sidebar'
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen gap-12 p-12">
       <Sidebar />
-      {children}
+      <div className="flex-1 space-y-12">
+        <SearchForm />
+        {children}
+      </div>
     </div>
   )
 }
---
+++ src/components/search-form.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { FormEvent } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export function SearchForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const data = Object.fromEntries(formData)
+    const query = data.q
+
+    if (!query) return null
+
+    router.push(`/search?q=${query}`)
+  }
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className={twMerge(
+        'group relative flex w-full max-w-64 items-center gap-4 border-b-2 py-2',
+        'border-transparent text-muted-foreground',
+        'transition-colors focus-within:border-border',
+      )}
+    >
+      <label htmlFor="search" className="sr-only">
+        Pesquisar
+      </label>
+
+      <Search />
+
+      <input
+        name="q"
+        id="search"
+        type="text"
+        defaultValue={query ?? ''}
+        placeholder="Procure em suas notas..."
+        className={twMerge(
+          'w-full bg-transparent text-foreground outline-none placeholder:text-muted-foreground',
+        )}
+        required
+      />
+    </form>
+  )
+}
