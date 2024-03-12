51ec6aa4a1dde2a30f4287812bff57e727702dc2
Juliano Sill <sill.juliano@gmail.com>
2024-03-11 21:33:02 -0300

    feat(components): create button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,47 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import { ComponentProps } from 'react'
+
+import { cn } from '@/utils/class-name-merge'
+
+const buttonVariants = cva(
+  'flex items-center justify-center rounded-md font-medium leading-none outline-none transition-colors focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-primary text-primary-foreground hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-accent text-accent-foreground hover:bg-primary hover:text-primary-foreground',
+        outline:
+          'bg-transparent text-primary border border-border hover:bg-accent hover:text-accent-foreground',
+        muted:
+          'bg-muted text-muted-foreground hover:bg-accent hover:text-accent-foreground',
+        ghost:
+          'bg-transparent text-foreground hover:bg-muted hover:text-strong',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive-foreground hover:text-destructive',
+      },
+      size: {
+        default: 'h-10 px-4 gap-2',
+        sm: 'h-8 px-3 gap-1.5 text-sm',
+        xs: 'h-6 px-2 gap-1 text-xs rounded',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+)
+
+type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants>
+
+export function Button({ className, variant, size, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
