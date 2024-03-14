1cb3024887ce96c60d3eabcc7d0f7119eee36873
Juliano Sill <sill.juliano@gmail.com>
2024-03-14 10:57:06 -0300

    feat(sidebar): fetch tags and render it conditionally

--- src/components/sidebar/tags.tsx
+++ src/components/sidebar/tags.tsx
@@ -1,6 +1,18 @@
-const tags = ['Typescript', 'Next.js', 'Node.js']
+'use client'
+
+import Link from 'next/link'
+
+import { useStore } from '@/app/store'
 
 export function Tags() {
+  const { notes } = useStore((store) => {
+    return { notes: store.notes }
+  })
+  const tags = notes?.map((note) => note.tags).flat()
+  const uniqueTags = Array.from(new Set(tags)).sort()
+
+  if (!uniqueTags || uniqueTags.length === 0) return null
+
   return (
     <section>
       <span className="text-sm font-semibold text-memonotes-300 dark:text-memonotes-400">
@@ -8,13 +20,14 @@ export function Tags() {
       </span>
 
       <div className="mt-6 flex flex-col items-start gap-4">
-        {tags.map((tag) => (
-          <button
+        {uniqueTags.map((tag) => (
+          <Link
             key={tag}
+            href={`/search?tag=${tag}`}
             className="text-sm font-medium text-zinc-200 hover:text-white dark:text-zinc-300 dark:hover:text-white"
           >
             {tag}
-          </button>
+          </Link>
         ))}
       </div>
     </section>
