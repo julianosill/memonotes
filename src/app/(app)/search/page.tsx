d2fea269dee74752f9bc6622578b7548a189a0a3
Juliano Sill <sill.juliano@gmail.com>
2024-03-14 13:53:54 -0300

    feat(search): create search page

--- src/app/(app)/search/page.tsx
+++ src/app/(app)/search/page.tsx
@@ -1,19 +1,50 @@
 'use client'
 
-import { useEffect } from 'react'
+import { redirect } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-import { useStore } from '@/app/store'
+import { INote, useStore } from '@/app/store'
 import { NoteCard } from '@/components/note-card'
 
-export default function Search() {
-  const { notes, fetchNotes } = useStore((store) => {
-    return { notes: store.notes, fetchNotes: store.fetchNotes }
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+export default function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+  if (!query) redirect('/')
+
+  const [notes, setNotes] = useState<INote[] | null>(null)
+
+  const { notes: storedNotes } = useStore((store) => {
+    return { notes: store.notes }
   })
+
+  useEffect(() => {
+    const filteredNotes = storedNotes?.filter((note) => {
+      return (
+        note.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+        note.content.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      )
+    })
+
+    if (!filteredNotes) return setNotes(null)
+
+    setNotes(filteredNotes)
+  }, [query, storedNotes])
 
   return (
     <main>
+      <header className="mb-8 border-b border-border-soft pb-2">
+        <p className="text-sm">
+          Exibindo resultados para:{' '}
+          <span className="font-medium text-strong">{query}</span>
+        </p>
+      </header>
       <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
-        {/* {notes?.map((note) => <NoteCard key={note.id} note={note} />)} */}
+        {notes?.map((note) => <NoteCard key={note.id} note={note} />)}
       </section>
     </main>
   )
