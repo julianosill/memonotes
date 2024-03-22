fbf16b7249a75666343c93afc2765f5b88f214ff
Juliano Sill <sill.juliano@gmail.com>
2024-03-21 23:30:22 -0300

    feat(tooltip): animate on hover

--- src/components/tooltip/content.tsx
+++ src/components/tooltip/content.tsx
@@ -1,27 +1,62 @@
 import * as Tooltip from '@radix-ui/react-tooltip'
+import { AnimatePresence, motion } from 'framer-motion'
 import { ForwardedRef, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
+
+import { useTooltip } from './root'
 
 export const Content = forwardRef(
   (
     { children, className, ...props }: Tooltip.TooltipContentProps,
     forwardedRef: ForwardedRef<HTMLDivElement>,
   ) => {
+    const isOpen = useTooltip()
+
+    const contentVariants = {
+      closed: { opacity: 0, y: 6 },
+      open: {
+        opacity: 1,
+        scale: 1,
+        y: 0,
+        transition: {
+          type: 'spring',
+          bounce: 0.5,
+          duration: 0.3,
+        },
+      },
+    }
+
     return (
-      <Tooltip.Portal>
-        <Tooltip.Content
-          ref={forwardedRef}
-          sideOffset={4}
-          className={twMerge(
-            'z-20 rounded-md bg-background px-3 py-2 text-sm shadow-lg ring-1 ring-border-soft',
-            className,
-          )}
-          {...props}
-        >
-          {children}
-          <Tooltip.Arrow width={12} height={6} className="fill-background" />
-        </Tooltip.Content>
-      </Tooltip.Portal>
+      <AnimatePresence>
+        {isOpen && (
+          <Tooltip.Portal forceMount>
+            <Tooltip.Content
+              asChild
+              ref={forwardedRef}
+              sideOffset={4}
+              className={twMerge(
+                'z-20 rounded-md bg-background px-3 py-2 text-sm shadow-lg ring-1 ring-border-soft',
+                className,
+              )}
+              {...props}
+            >
+              <motion.div
+                variants={contentVariants}
+                initial="closed"
+                animate="open"
+                exit="closed"
+              >
+                {children}
+                <Tooltip.Arrow
+                  width={12}
+                  height={6}
+                  className="fill-background"
+                />
+              </motion.div>
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        )}
+      </AnimatePresence>
     )
   },
 )
--- src/components/tooltip/root.tsx
+++ src/components/tooltip/root.tsx
@@ -1,9 +1,20 @@
 import * as Tooltip from '@radix-ui/react-tooltip'
+import { createContext, useContext, useState } from 'react'
 
-export function Root({ children }: Tooltip.TooltipProps) {
+const TooltipContext = createContext(false)
+
+export function Root({ children, ...props }: Tooltip.TooltipProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <Tooltip.Provider delayDuration={400}>
-      <Tooltip.Root>{children}</Tooltip.Root>
-    </Tooltip.Provider>
+    <TooltipContext.Provider value={isOpen}>
+      <Tooltip.Provider delayDuration={400}>
+        <Tooltip.Root onOpenChange={setIsOpen} {...props}>
+          {children}
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    </TooltipContext.Provider>
   )
 }
+
+export const useTooltip = () => useContext(TooltipContext)
