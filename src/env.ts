e3c3c525e0288277f40ad74fa47cd2da69a3a7f0
Juliano Sill <sill.juliano@gmail.com>
2024-04-01 13:27:07 -0300

    feat(firebase): connect to database

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    API_KEY: z.string(),
+    AUTH_DOMAIN: z.string(),
+    PROJECT_ID: z.string(),
+    STORAGE_BUCKET: z.string(),
+    MESSAGING_SENDER_ID: z.string(),
+    APP_ID: z.string(),
+    MEASUREMENT_ID: z.string(),
+  },
+  runtimeEnv: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  },
+})
