c7446d787ea710ffd71890a44e79430f271dc5c5
Juliano Sill <sill.juliano@gmail.com>
2024-04-03 19:52:09 -0300

    style(layout): update responsive gap

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,22 +3,19 @@ import { twMerge } from 'tailwind-merge'
 
 import { Sidebar } from '@/components/sidebar'
 
-import { FetchNotes } from './fetch-notes'
-
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div
       className={twMerge(
         'flex min-h-screen flex-1 p-6',
-        'md:max-lg:gap-8',
-        'md:grid md:grid-cols-app md:gap-12 md:p-12',
+        'md:grid md:grid-cols-app md:gap-8 md:p-12',
+        'lg:gap-12',
       )}
     >
       <Sidebar />
       <div className="flex w-full flex-col pt-14 md:col-start-2 md:pt-0">
         {children}
       </div>
-      <FetchNotes />
     </div>
   )
 }
