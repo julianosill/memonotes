3f05b6c643cb7c67d3e601c424f92f8885b5bfb2
Juliano Sill <sill.juliano@gmail.com>
2024-04-13 17:18:23 -0300

    feat(note-form): add bubble toolbar text formatting

--- src/components/note-form/_components/content-editor/index.tsx
+++ src/components/note-form/_components/content-editor/index.tsx
@@ -1,131 +1,23 @@
-import * as Toolbar from '@radix-ui/react-toolbar'
-import { EditorContent, EditorContentProps } from '@tiptap/react'
-import {
-  Bold,
-  Heading2,
-  Heading3,
-  Heading4,
-  Italic,
-  List,
-  ListOrdered,
-  Pilcrow,
-  Quote,
-  SeparatorHorizontal,
-} from 'lucide-react'
+import { BubbleMenu, EditorContent, EditorContentProps } from '@tiptap/react'
 
 import { cn } from '@/utils/class-name-merge'
 
-import { ToolbarButton } from './toolbar-button'
-import { ToolbarGroup } from './toolbar-group'
+import { ToolbarMenu } from './toolbar-menu'
 
 export function ContentEditor({ editor }: EditorContentProps) {
   return (
     <section className="flex h-full w-full flex-col gap-4 p-5">
-      <Toolbar.Root
-        aria-label="Opções de formatação"
-        className="flex w-fit flex-wrap justify-center gap-x-6 gap-y-3 rounded-md border border-border-soft px-3 py-2 text-muted-foreground"
-      >
-        <ToolbarGroup type="multiple" aria-label="Formatação de texto">
-          <ToolbarButton
-            aria-label="Negrito"
-            title="Negrito"
-            data-state={editor?.isActive('bold') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().toggleBold().run()}
-          >
-            <Bold className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Itálico"
-            title="Itálico"
-            data-state={editor?.isActive('italic') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().toggleItalic().run()}
-          >
-            <Italic className="size-3.5" />
-          </ToolbarButton>
-        </ToolbarGroup>
+      <ToolbarMenu editor={editor} />
 
-        <ToolbarGroup type="multiple" aria-label="Estrutura de texto">
-          <ToolbarButton
-            aria-label="Lista não-ordenada"
-            title="Lista não-ordenada"
-            data-state={editor?.isActive('bulletList') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().toggleBulletList().run()}
-          >
-            <List className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Lista ordenada"
-            title="Lista ordenada"
-            data-state={editor?.isActive('orderedList') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().toggleOrderedList().run()}
-          >
-            <ListOrdered className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Citação"
-            title="Citação"
-            data-state={editor?.isActive('blockquote') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().toggleBlockquote().run()}
-          >
-            <Quote className="size-3.5" />
-          </ToolbarButton>
-        </ToolbarGroup>
-
-        <ToolbarGroup type="multiple" aria-label="Composição de texto">
-          <ToolbarButton
-            aria-label="Cabeçalho 2"
-            title="Cabeçalho 2"
-            data-state={
-              editor?.isActive('heading', { level: 2 }) ? 'on' : 'off'
-            }
-            onClick={() =>
-              editor?.chain().focus().setHeading({ level: 2 }).run()
-            }
-          >
-            <Heading2 className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Cabeçalho 3"
-            title="Cabeçalho 3"
-            data-state={
-              editor?.isActive('heading', { level: 3 }) ? 'on' : 'off'
-            }
-            onClick={() =>
-              editor?.chain().focus().setHeading({ level: 3 }).run()
-            }
-          >
-            <Heading3 className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Cabeçalho 4"
-            title="Cabeçalho 4"
-            data-state={
-              editor?.isActive('heading', { level: 4 }) ? 'on' : 'off'
-            }
-            onClick={() =>
-              editor?.chain().focus().setHeading({ level: 4 }).run()
-            }
-          >
-            <Heading4 className="size-3.5" />
-          </ToolbarButton>
-          <ToolbarButton
-            aria-label="Parágrafo"
-            title="Parágrafo"
-            data-state={editor?.isActive('paragraph') ? 'on' : 'off'}
-            onClick={() => editor?.chain().focus().setParagraph().run()}
-          >
-            <Pilcrow className="size-3.5" />
-          </ToolbarButton>
-        </ToolbarGroup>
-
-        <ToolbarButton
-          aria-label="Separador"
-          title="Separador"
-          onClick={() => editor?.chain().focus().setHorizontalRule().run()}
+      {editor && (
+        <BubbleMenu
+          editor={editor}
+          tippyOptions={{ duration: 100, maxWidth: 'fit' }}
+          className="rounded-md bg-card shadow-md"
         >
-          <SeparatorHorizontal className="size-3.5" />
-        </ToolbarButton>
-      </Toolbar.Root>
+          <ToolbarMenu editor={editor} />
+        </BubbleMenu>
+      )}
 
       <EditorContent
         editor={editor}
